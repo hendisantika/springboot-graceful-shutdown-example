23c11ec92eb8d04b01399b1df00ea0403448bd3d
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-04 10:20:52 +0700

    #4 - Add Thread Pool which runs some tasks

--- src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
+++ src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
@@ -3,8 +3,13 @@ package com.hendisantika.springbootgracefulshutdownexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 @SpringBootApplication
 public class SpringbootGracefulShutdownExampleApplication {
+    //Thread Pool which runs some tasks
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGracefulShutdownExampleApplication.class, args);
