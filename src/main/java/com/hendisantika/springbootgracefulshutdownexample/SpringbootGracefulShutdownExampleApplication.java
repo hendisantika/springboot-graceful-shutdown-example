54cc439304a029f9f0261545a7384cb6ab2fcd6c
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-04 10:23:13 +0700

    #8 - Add @PreDestroy

--- src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
+++ src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
@@ -1,16 +1,19 @@
 package com.hendisantika.springbootgracefulshutdownexample;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import static java.lang.Thread.sleep;
 
+@Log4j2
 @SpringBootApplication
 public class SpringbootGracefulShutdownExampleApplication {
     //Thread Pool which runs some tasks
@@ -20,12 +23,27 @@ public class SpringbootGracefulShutdownExampleApplication {
         SpringApplication.run(SpringbootGracefulShutdownExampleApplication.class, args);
     }
 
+    @PreDestroy
+    public void destroy() {
+        log.info("Triggered PreDestroy");
+
+        //Verify if the threads have completed their tasks and then proceed with shutdown
+        while (executor.getActiveCount() > 0) {
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("Completed all active threads");
+    }
+
     @RestController
     class UserController {
 
         @GetMapping("/hello")
         public String hello() throws InterruptedException {
-            System.out.println("Waiting for 5 seconds...");
+            log.info("Waiting for 5 seconds...");
             executor.execute(this::task);
             return "Hello World! " + LocalDateTime.now();
         }
@@ -36,7 +54,7 @@ public class SpringbootGracefulShutdownExampleApplication {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Completed 5 seconds...");
+            log.info("Completed 5 seconds...");
         }
     }
 
