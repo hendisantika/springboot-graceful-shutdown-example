c0c8105011452915b7303de591274811b36f6e50
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-04 10:19:01 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-graceful-shutdown-example
---
+++ src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgracefulshutdownexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGracefulShutdownExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGracefulShutdownExampleApplication.class, args);
+    }
+
+}
