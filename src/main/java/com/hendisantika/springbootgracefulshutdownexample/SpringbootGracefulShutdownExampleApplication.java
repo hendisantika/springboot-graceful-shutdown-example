8584b45f3cd3fb8bd53137d4ccbe060790071b1c
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-04 10:21:15 +0700

    #5 - Add UserController

--- src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
+++ src/main/java/com/hendisantika/springbootgracefulshutdownexample/SpringbootGracefulShutdownExampleApplication.java
@@ -2,9 +2,13 @@ package com.hendisantika.springbootgracefulshutdownexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.lang.Thread.sleep;
 
 @SpringBootApplication
 public class SpringbootGracefulShutdownExampleApplication {
@@ -13,6 +17,26 @@ public class SpringbootGracefulShutdownExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGracefulShutdownExampleApplication.class, args);
+    }
+
+    @RestController
+    class UserController {
+
+        @GetMapping("/hello")
+        public String hello() throws InterruptedException {
+            System.out.println("Waiting for 5 seconds...");
+            executor.execute(this::task);
+            return "Hello World";
+        }
+
+        private void task() {
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Completed 5 seconds...");
+        }
     }
 
 }
